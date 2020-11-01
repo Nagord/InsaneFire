e45c813c1654295bc540d7051c3e6b4f888b3fd5
DragonFire47 <1redyoung@gmail.com>
2020-11-01 13:37:48 -0500

    modification to log description

--- InsaneFire/Plugin.cs
+++ InsaneFire/Plugin.cs
@@ -13,6 +13,7 @@ namespace InsaneFire
         public override string LongDescription => "-Modifies fire cap from 20 to 10,000." +
             "\n-Makes fire nodes spread more than once, as opposed to once per node, removing the snake effect" +
             "\n-Modifies oxygen consumption per fire." +
+            "\n-Syncs oxygen consumption between players with mod." +
             "\n-Provides Commands for customization of the mod" +
             "\n-Allows toggling between moddified fire and vanilla fire" +
             "\n-Saves and loads settings automatically.";
