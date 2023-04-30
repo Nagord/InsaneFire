2fced1287480b3d3a0af3593b152088a70131779
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2023-04-30 16:20:21 -0700

    Added LineCasting, however the Feature doesn't optimize as much as I'd like.

--- InsaneFire/InsaneFire.cs
+++ InsaneFire/InsaneFire.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 using static PulsarModLoader.Patches.HarmonyHelpers;
@@ -9,8 +10,30 @@ namespace InsaneFire
     [HarmonyPatch(typeof(PLFire), "Update")]
     class MainUpdatePatch
     {
+        public static Dictionary<PLFire, bool> RegisteredFires = new Dictionary<PLFire, bool>();
+        static bool PatchMethod(PLRoomArea roomArea, PLFire inFire)
+        {
+            bool Succession = roomArea.IsVisible() && !Physics.Linecast(PLCameraSystem.GetCenterEyeAnchor().position, inFire.transform.position);
+            RegisteredFires.Add(inFire, Succession);
+            return Succession;
+        }
+
+        static bool PatchMethodSuccession(PLRoomArea roomArea, PLFire inFire)
+        {
+            return RegisteredFires[inFire];
+        }
+
+        static bool PatchMethodEnd(PLRoomArea roomArea, PLFire inFire)
+        {
+            bool Succession = RegisteredFires[inFire];
+            RegisteredFires.Remove(inFire);
+            return Succession;
+        }
+
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
+
+            //Fire limit
             List<CodeInstruction> targetSequence = new List<CodeInstruction>()
             {
                 new CodeInstruction(OpCodes.Ldc_I4_S, (sbyte)20),
@@ -24,7 +47,7 @@ namespace InsaneFire
             instructions = PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
 
 
-
+            //Oxy Consumption
             targetSequence = new List<CodeInstruction>()
             {
                 new CodeInstruction(OpCodes.Ldc_R4, 0.0005f),
@@ -33,6 +56,41 @@ namespace InsaneFire
             injectedSequence = new List<CodeInstruction>()
             {
                 new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(Global), "O2Consumption")),
+            };
+
+            instructions = PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
+
+
+            MethodInfo visibleMethod = AccessTools.Method(typeof(PLRoomArea), "IsVisible");
+            MethodInfo PatchMethodStart = AccessTools.Method(typeof(MainUpdatePatch), "PatchMethod");
+            MethodInfo PatchMethodSuccession = AccessTools.Method(typeof(MainUpdatePatch), "PatchMethodSuccession");
+            MethodInfo PatchMethodEnd = AccessTools.Method(typeof(MainUpdatePatch), "PatchMethodEnd");
+            targetSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Callvirt, visibleMethod),
+            };
+
+            injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, PatchMethodStart),
+            };
+
+            instructions = PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
+
+            injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, PatchMethodSuccession),
+            };
+
+            instructions = PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
+            instructions = PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
+
+            injectedSequence = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, PatchMethodEnd),
             };
 
             return PatchBySequence(instructions, targetSequence, injectedSequence, patchMode: PatchMode.REPLACE);
